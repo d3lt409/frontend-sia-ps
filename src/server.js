a77a8e66d3965d2bd000484404918e7f1f23dcee
d3lt409 <manuelfer1996@gmail.com>
2022-01-13 06:53:51 -0500

    Ajuestes Final

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ng-blog'));
-app.get('/', function(req, res) {
+// Serve static files
+app.use(express.static(__dirname + '/dist/fronted-sia-unal'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/fronted-sia-unal/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
